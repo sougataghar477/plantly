d88b7bae89d24008e125721ebdf473ed397d5bb1
sougata <sougataghar47@gmail.com>
2024-11-28 21:47:45 +0530

    added mobile navigation

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -5,28 +5,39 @@ import {
     DrawerBody,
     DrawerCloseTrigger,
     DrawerContent,
-
-    DrawerHeader,
     DrawerRoot,
-    DrawerTitle,
+ 
 
 } from "@/components/ui/drawer";
-import { Flex } from "@chakra-ui/react";
+import { Flex,Icon,Box } from "@chakra-ui/react";
+import { HiShoppingCart } from "react-icons/hi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import { useContext } from "react";
+import { SearchContext } from "@/context/SearchContext";
 function MobileNavbar({ open, setOpen }) {
     const path = usePathname();
-
+    let { status } = useSession()
+    let {cart}=useContext(SearchContext)
     return <DrawerRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
         <DrawerBackdrop />
         <DrawerContent>
             <DrawerBody>
-                <Flex flexDirection={'column'} gap={4}>
+                <Flex mt={8} flexDirection={'column'} gap={4}>
                     <Link onClick={()=> setOpen(false)} className={path === '/' ? 'active-link' : ''} href={'/'}>Plantly</Link>
                     <Link onClick={()=> setOpen(false)} className={path === '/herbs' ? 'active-link' : ''} href={'/herbs'}>Herbs</Link>
                     <Link onClick={()=> setOpen(false)} className={path === '/shrubs' ? 'active-link' : ''} href={'/shrubs'}>Shrubs</Link>
                     <Link onClick={()=> setOpen(false)} className={path === '/trees' ? 'active-link' : ''} href={'/trees'}>Trees</Link>
+                    <Link onClick={()=> setOpen(false)} className={path === '/user' ? 'active-link' : ''} href={'/user'}>User Profile</Link>
+                    {status === 'unauthenticated' && <Link className={path === '/signin' ? 'active-link' : ''} onClick={()=> setOpen(false)} href={'signin'}>Sign In</Link>}
+                    {status === 'unauthenticated' && <Link className={path === '/signup' ? 'active-link' : ''} onClick={()=> setOpen(false)} href={'signup'}>Sign Up</Link>}
+                    <Link className={path === '/cart' ? 'active-link' : ''} onClick={()=> setOpen(false)} href={'cart'}><Box display={'inline'} pos={'relative'}>
+                        <Icon fontSize="2xl"><HiShoppingCart /></Icon>
+                        {cart.length > 0 && <span className="cartIconLength">{cart.length}</span>}
+                    </Box></Link>
+                    {status === 'authenticated' && <span onClick={() => signOut()}>Sign Out</span>}
                 </Flex>
             </DrawerBody>
 
