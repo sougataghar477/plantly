a97684cf3463fa04715cfb5cf48dfb35897b4831
sougata <sougataghar47@gmail.com>
2024-11-29 16:24:40 +0530

    revalidating api routes

--- app/api/get_items/route.js
+++ app/api/get_items/route.js
@@ -2,7 +2,7 @@
 import db from "@/mongo";
 export async function GET(req) {
     const itemsCollection =await db.collection("items").find().toArray();
-    const pathsToRevalidate = ["/", "/herbs", "/shrubs", "/trees"];
+    const pathsToRevalidate = ["/", "/herbs", "/shrubs", "/trees","/search"];
 
   // Revalidate each path
   pathsToRevalidate.forEach((path) => {
