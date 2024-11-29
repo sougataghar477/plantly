523df826533ddcf20d55ca6dcdaa481d88f79a47
sougata <sougataghar47@gmail.com>
2024-11-29 16:11:43 +0530

    data hydrated

--- app/api/get_items/route.js
+++ app/api/get_items/route.js
@@ -1,7 +1,8 @@
- 
+ import { revalidatePath } from "next/cache";
 import db from "@/mongo";
 export async function GET(req) {
-    const itemsCollection =await db.collection("items").find().toArray();   
+    const itemsCollection =await db.collection("items").find().toArray();
+    revalidatePath('/')   
     return Response.json({items:itemsCollection})
  
   }
