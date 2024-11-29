e78ab81ea2b5bfa8554ca790f40e51238300e0aa
sougata <sougataghar47@gmail.com>
2024-11-29 15:41:12 +0530

    data hydrated

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,17 @@
 import { getItems } from "@/GetItems";
 import { Container, Heading, Box, Flex } from "@chakra-ui/react";
 import GreenCard from "@/components/Card";
-import db from "@/mongo";
+
 
 async function Home() {
-  // let { items, error, loading } = await getItems();
-  let items =await db.collection("items").find().toArray();
+  let { items, error, loading } = await getItems();
+
 
   return <Container py={16} px={[4, 16, 16]} maxW={1260}>
     <Box maxW={940} mx={'auto'}>
       <Heading mb={8} fontSize={'4xl'}>Items</Heading>
       <Flex mx={'auto'} justifyContent={'center'} gap={'20px'} wrap={'wrap'} >
-        {/* {loading ? (
+        {loading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>{error}</p>
@@ -25,16 +25,8 @@ async function Home() {
               imageUrl={item.imageUrl}
             />
           ))
-        )} */}
-{items.map((item, index) => (
-            <GreenCard
-              key={index}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              imageUrl={item.imageUrl}
-            />
-          ))}
+        )}
+
       </Flex>
     </Box>
 
