02e4e09b133954594070f6fc583239c6d24e4e1f
sougata <sougataghar47@gmail.com>
2024-11-29 13:47:41 +0530

    data hydrated from api

--- GetItems.js
+++ GetItems.js
@@ -4,7 +4,7 @@ export async function getItems(category){
     let loading=false;
     try{
       loading=true;  
-      const response = await fetch('http://localhost:3000/api/get_items');
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get_items`);
       
       if (!response.ok) {
         throw new Error(`Failed to fetch data: ${response.status}`);
